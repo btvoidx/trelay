5775598ea76d3f2786fa82263d51636c8110b9b3
Max Tumozov <maksimtumozov@gmail.com>
2022-11-20 21:22:27 +0400

    Move out disconnect messages into variables

--- trelay.go
+++ trelay.go
@@ -9,17 +9,23 @@ func ListenAndServe(addr string, h SessionHandler) error {
 	return (&Server{Addr: addr, Handler: h}).ListenAndServe()
 }
 
+var (
+	ConnectionFailed = "trelay: couldn't connect to remote server"
+	ServerIsFull     = "trelay: server is full"
+)
+
 // A simplest SessionHandler. Connects the client to the remote server at specified address and makes sure
 // total player count is not exceeded.
+// Default disconnect messages can be changed by changing ConnectionFailed and ServerIsFull global variables.
 func Direct(addr string, maxPlayers int64) SessionHandler {
 	dcPacket := (&Writer{}).SetId(2).
 		WriteByte(0).
-		WriteStringf("trelay: couldn't connect to %s", addr).
+		WriteString(ConnectionFailed).
 		Data()
 
 	maxPlayersPacket := (&Writer{}).SetId(2).
 		WriteByte(0).
-		WriteString("trelay: server is full").
+		WriteString(ServerIsFull).
 		Data()
 
 	currentPlayers := atomic.Int64{}
